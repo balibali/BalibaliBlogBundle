6ca68290db44cad3caf0afa6c66087639cd76cbd
Rimpei Ogawa <ogawa@tejimaya.com>
2010-09-24 00:26:17 +0900

    fix tests

---
+++ Tests/Controller/PostControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bundle\Balibali\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PostControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Recent Posts")')->count() > 0);
+    }
+
+    public function testManage()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/backend/');
+
+        $this->assertTrue($crawler->filter('html:contains("Manage Posts")')->count() > 0);
+    }
+
+    public function testNew()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/backend/posts/new');
+
+        $this->assertTrue($crawler->filter('html:contains("New Post")')->count() > 0);
+    }
+}
