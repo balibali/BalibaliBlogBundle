2406e6d3373e464ee94d41d079e75c9a05f5c59a
Rimpei Ogawa <ogawa@tejimaya.com>
2010-09-23 22:48:08 +0900

    refactor controller

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -17,31 +17,23 @@ class BackendController extends Controller
             ->getRepository('BlogBundle:Post')->createQuery()
             ->sort('publishedAt', 'desc')
             ->execute();
-        $form = new Form('delete', new \stdClass, $this['validator']);
+        $form = $this->getDeleteForm();
 
         return $this->render('Balibali/BlogBundle:Backend:index:twig', array('posts' => $posts, 'form' => $form));
     }
 
     public function newAction()
     {
-        $post = new Post();
-        $form = new PostForm('post', $post, $this['validator']);
+        $form = $this->getPostForm();
 
         return $this->render('Balibali/BlogBundle:Backend:new:twig', array('form' => $form));
     }
 
     public function createAction()
     {
-        $post = new Post();
-        $form = new PostForm('post', $post, $this['validator']);
+        $form = $this->getPostForm();
 
-        $form->bind($this['request']->request->get($form->getName()));
-
-        if ($form->isValid()) {
-            $dm = $this['doctrine.odm.mongodb.document_manager'];
-            $dm->persist($post);
-            $dm->flush();
-
+        if ($this->processPostForm($form)) {
             return $this->redirect($this->generateUrl('balibali_blog_backend_index', array(), true));
         }
 
@@ -50,36 +42,16 @@ class BackendController extends Controller
 
     public function editAction($id)
     {
-        $post = $this['doctrine.odm.mongodb.document_manager']
-            ->find('BlogBundle:Post', $id);
-
-        if (!$post) {
-            throw new NotFoundHttpException('The post does not exist.');
-        }
-
-        $form = new PostForm('post', $post, $this['validator']);
+        $form = $this->getPostForm($id);
 
         return $this->render('Balibali/BlogBundle:Backend:edit:twig', array('form' => $form));
     }
 
     public function updateAction($id)
     {
-        $post = $this['doctrine.odm.mongodb.document_manager']
-            ->find('BlogBundle:Post', $id);
+        $form = $this->getPostForm($id);
 
-        if (!$post) {
-            throw new NotFoundHttpException('The post does not exist.');
-        }
-
-        $form = new PostForm('post', $post, $this['validator']);
-
-        $form->bind($this['request']->request->get($form->getName()));
-
-        if ($form->isValid()) {
-            $dm = $this['doctrine.odm.mongodb.document_manager'];
-            $dm->persist($post);
-            $dm->flush();
-
+        if ($this->processPostForm($form)) {
             return $this->redirect($this->generateUrl('balibali_blog_backend_index', array(), true));
         }
 
@@ -88,17 +60,11 @@ class BackendController extends Controller
 
     public function deleteAction($id)
     {
-        $post = $this['doctrine.odm.mongodb.document_manager']
-            ->find('BlogBundle:Post', $id);
+        $post = $this->getPostById($id);
 
-        if (!$post) {
-            throw new NotFoundHttpException('The post does not exist.');
-        }
-
-        $form = new Form('delete', new \stdClass, $this['validator']);
-
+        // CSRF protection
+        $form = $this->getDeleteForm();
         $form->bind($this['request']->request->get($form->getName()));
-
         if (!$form->isValid()) {
             throw new ForbiddenHttpException('CSRF token is not matched.');
         }
@@ -108,5 +74,48 @@ class BackendController extends Controller
         $dm->flush();
 
         return $this->redirect($this->generateUrl('balibali_blog_backend_index', array(), true));
+    }
+
+    protected function getPostById($id)
+    {
+        $post = $this['doctrine.odm.mongodb.document_manager']
+            ->find('BlogBundle:Post', $id);
+
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+
+        return $post;
+    }
+
+    protected function getPostForm($id = null)
+    {
+        if ($id === null) {
+            $post = new Post();
+        } else {
+            $post = $this->getPostById($id);
+        }
+
+        return new PostForm('post', $post, $this['validator']);
+    }
+
+    protected function processPostForm($form)
+    {
+        $form->bind($this['request']->request->get($form->getName()));
+
+        if ($form->isValid()) {
+            $dm = $this['doctrine.odm.mongodb.document_manager'];
+            $dm->persist($form->getData());
+            $dm->flush();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getDeleteForm()
+    {
+        return new Form('delete', new \stdClass, $this['validator']);
     }
 }
